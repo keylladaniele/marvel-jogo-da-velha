456af23038e4b6064eba5ea92e37a919b94c6aa2
Keylla Daniele <93086847+keylladaniele@users.noreply.github.com>
2022-03-19 22:28:37 -0300

    progresso

--- src/app/services/marvel-api.service.ts
+++ src/app/services/marvel-api.service.ts
@@ -14,7 +14,7 @@ export class MarvelApiService {
 
   publicKey = 'a5ae98290a71aa3e65e9e72530c60cbc';
   hash = '2f15f9c35a6aa8e18b9e220bf94f6e6e';
-  urlApi = `https://gateway.marvel.com/v1/public/characters?limit=10&ts=1000&apikey=${this.publicKey}&hash=${this.hash}`;
+  urlApi = `https://gateway.marvel.com/v1/public/characters?limit=6&ts=1000&apikey=${this.publicKey}&hash=${this.hash}`;
 
 
   getAllCharacters():Observable<any>{
@@ -22,9 +22,15 @@ export class MarvelApiService {
     
   } 
   
-  getCharacterByName(characterName:string):Observable<any>
+  getCharacterByName1(characterName:string):Observable<any>
   {
-    const characterByNameUrl = `https://gateway.marvel.com:443/v1/public/characters?&nameStartsWith=${characterName}&ts=1000&apikey=${this.publicKey}&hash=${this.hash}`;
+    const characterByNameUrl = `https://gateway.marvel.com:443/v1/public/characters?limit=1&nameStartsWith=${characterName}&ts=1000&apikey=${this.publicKey}&hash=${this.hash}`;
+    return this.http.get(characterByNameUrl);
+  }
+
+  getCharacterByName2(characterName:string):Observable<any>
+  {
+    const characterByNameUrl = `https://gateway.marvel.com:443/v1/public/characters?limit=1&nameStartsWith=${characterName}&ts=1000&apikey=${this.publicKey}&hash=${this.hash}`;
     return this.http.get(characterByNameUrl);
   }
 
